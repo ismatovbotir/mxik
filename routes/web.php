483069b2d2e52e794cc5a5e718147b01d1ebacb3
Botir Ismatov <botirismatov@MacBook-Pro-Botir.local>
2025-08-29 23:20:34 +0500

    itemscds

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('/', MainController::class);
-Route::resource('/product',ProductController)
+Route::resource('/product', ProductController::class);
