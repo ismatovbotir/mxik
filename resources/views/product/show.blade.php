17e50a23452affcca547ce201988d96fd86e2422
Botir Ismatov <botirismatov@MacBook-Pro-Botir.local>
2025-08-29 23:26:00 +0500

    fewqregergqerg

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -7,6 +7,10 @@
   <title>Product</title>
 </head>
 <body>
-  <img src="https://tasnif.soliq.uz/api/cls-api/integration-mxik/references/get/file/{{$id}}_1.png" alt="">
+  <div class="!w-full !h-[22rem] flex-shrink-0 p-2 !flex !items-center !justify-center overflow-hidden bg-white cursor-pointer hover:border-blue-default duration-200" tabindex="-1" style="width: 100%; display: inline-block;">
+    <img width="100%" height="100%" class="object-contain" src="https://tasnif.soliq.uz/api/cls-api/integration-mxik/references/get/file/{{$id}}_1.png">
+  </div>
+  
+            
 </body>
 </html>
