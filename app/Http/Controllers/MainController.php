d17655f7c863ef1087ffe7a227857253ea8aa68f
Botir Ismatov <botirismatov@MacBook-Pro-Botir.local>
2025-08-31 23:38:52 +0500

    sdfsfergregerte

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -22,10 +22,10 @@ class MainController extends Controller
             $productsByCountry = Product::whereNotNull('gtin_id')
                 ->join('gtins', 'products.gtin_id', '=', 'gtins.id')
                 ->join('groups', 'products.group_id', '=', 'groups.id')
-                ->select('gtins.nameEn', 'gtins.id', DB::raw('COUNT(products.id) as total'))
-                ->groupBy('gtins.nameEn', 'gtins.id')
+                ->select('gtins.nameEn', DB::raw('COUNT(products.id) as total'))
+                ->groupBy('gtins.nameEn')
                 ->orderByDesc('total')
-                ->paginate(20)->toArray();
+                ->paginate(50)->toArray();
 
 
 
