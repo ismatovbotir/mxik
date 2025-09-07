a1a10df2644b3aea612da9be7e775540a9c38b68
Botir Ismatov <botirismatov@MacBook-Pro-Botir.local>
2025-09-07 18:46:44 +0500

    sdffqwefrgtrhyhytte

--- app/Console/Commands/TasnifCode.php
+++ app/Console/Commands/TasnifCode.php
@@ -125,7 +125,7 @@ class TasnifCode extends Command
                         'record_total' => count($jsonArr["data"]) + $page->record_total
                     ]
                 );
-                $this->telegramSend('Data upserted successfully : ' . $page->total);
+                $this->telegramSend('Data upserted successfully : ' . $page->record_total);
             } else {
                 $this->telegramSend("Request failed with status: {$response->status()} : {{$response->body()}}");
             }
