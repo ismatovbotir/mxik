3e7bef89b0707005290fe570f81233e2bda5f543
Botir Ismatov <botirismatov@MacBook-Pro-Botir.local>
2025-09-09 23:36:30 +0500

    erferfqerfdfvgd

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,4 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-//Route::get('/product/{barcode}',)
+Route::get('/product',ProductController::class);
