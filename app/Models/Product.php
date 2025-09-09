d679a062def2d44071f7c2b9d0785fe3ef3ce4eb
Botir Ismatov <botirismatov@MacBook-Pro-Botir.local>
2025-09-09 23:50:20 +0500

    sdfsdffwertrbrb

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Gtin;
 use App\Models\Group;
+use App\Models\Unit;
 
 
 class Product extends Model
@@ -20,5 +21,9 @@ class Product extends Model
     public function group()
     {
         return $this->belongsTo(Group::class, 'group_id', 'id');
+    }
+    public function units()
+    {
+        return $this->hasMany(Unit::class);
     }
 }
--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
 
 class Unit extends Model
 {
     public $timestamps = false;
     public $guarded = [];
+
+    public function product()
+    {
+        return $this->belongsTo( Product::class);
+    }
 }
