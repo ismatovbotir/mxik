20b496b824aeb13f1a0f01d9729dca49b9ac992d
Botir Ismatov <botirismatov@MacBook-Pro-Botir.local>
2025-09-09 23:38:26 +0500

    product api resource

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/product',ProductController::class);
+Route::resource('/product',ProductController::class);
