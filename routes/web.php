a35f1f0fdf9abfcc3da50e94b47a5794a8d50cf9
Botir Ismatov <botirismatov@MacBook-Pro-Botir.local>
2025-09-10 13:43:53 +0500

    groupController

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\GroupController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::resource('/', MainController::class);
 Route::resource('/product', ProductController::class);
+Route::resource('/group',GroupController::class);
